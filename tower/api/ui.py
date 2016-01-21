b20a845f58e7c5e61fe80422a2e4b71637e8676f
Dmitry Volodin <dv@nocproject.org>
2016-01-22 00:50:16 +0300

    Better caching of bundled JS and CSS assets

--- tower/api/ui.py
+++ tower/api/ui.py
@@ -35,7 +35,8 @@ class UIHandler(tornado.web.RequestHandler):
         return self.render(
             os.path.join(self.root, "index.html"),
             installation_name=name,
-            mergecache=self.mergecache
+            mergecache=self.mergecache,
+            hashed=self.hashed
         )
 
     def mergecache(self, jslist):
@@ -59,3 +60,17 @@ class UIHandler(tornado.web.RequestHandler):
                 with open(cache_path, "w") as f:
                     f.write(js)
         return "<script src=\"/ui/cache/%s.js\" type=\"text/javascript\"></script>" % self.hash
+
+    def hashed(self, path):
+        """
+        Convert path to path?hash version
+        :param path:
+        :return:
+        """
+        fp = path
+        if fp.startswith("/ui/"):
+            fp = fp[4:]
+        fp = os.path.join(self.root, fp)
+        with open(fp) as f:
+            hash = hashlib.sha256(f.read()).hexdigest()[:8]
+        return "%s?%s" % (path, hash)
