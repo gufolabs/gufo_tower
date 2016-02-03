37dd17c8215a11e9e3a8c69b006f6075368837e5
Dmitry Volodin <dv@nocproject.org>
2016-02-04 01:37:25 +0300

    0.1a28

---
+++ tower/migrations/015_environment_influx.py
@@ -0,0 +1,21 @@
+from peewee import CharField
+
+
+def migrate(migrator):
+    migrator.add_column(
+        "environment",
+        "influxdb_db",
+        CharField(default="noc")
+    )
+
+    migrator.add_column(
+        "environment",
+        "influxdb_user",
+        CharField(default="noc")
+    )
+
+    migrator.add_column(
+        "environment",
+        "influxdb_password",
+        CharField(default="noc")
+    )
