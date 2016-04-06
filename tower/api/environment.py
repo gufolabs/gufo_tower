37aedd3a3a41178b5b4f988eb7751c1755a477fc
Dmitry Volodin <dv@nocproject.org>
2016-04-06 14:40:52 +0300

    Fix YaML inventory dump format

--- tower/api/environment.py
+++ tower/api/environment.py
@@ -20,4 +20,5 @@ class EnvironmentAPI(ModelAPI):
     @api
     def ansible_inventory(self, env_id):
         e = Environment.get(Environment.id == int(env_id))
-        return yaml.dump(e.ansible_inventory())
+        return yaml.safe_dump(e.ansible_inventory(),
+                              default_flow_style=False)
