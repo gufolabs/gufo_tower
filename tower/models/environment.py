3bfa81f37a6a65d99b11457737f9fded87c3a795
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-04-13 12:40:57 +0300

    Fix generate SSH key for FreeBSD

--- tower/models/environment.py
+++ tower/models/environment.py
@@ -462,11 +462,18 @@ class Environment(Model):
                 if not os.path.isfile(fn):
                     logging.info("Generating %s key for pool %s",
                                  t, pool.name)
-                    subprocess.check_call(
-                        ["ssh-keygen", "-q", "-t", t, "-b", str(b),
-                         "-f", fn,
-                         "-N", "", "-C", "%s@noc" % pool.name]
-                    )
+                    if os.getenv("OSTYPE") == "FreeBSD":
+                        subprocess.check_call(
+                            ["ssh-keygen", "-q", "-t", t, "-b", str(b),
+                            "-f", fn,
+                            "-N", "\"\"", "-C", "%s@noc" % pool.name]
+                        )
+                    else:
+                        subprocess.check_call(
+                            ["ssh-keygen", "-q", "-t", t, "-b", str(b),
+                            "-f", fn,
+                            "-N", "", "-C", "%s@noc" % pool.name]
+                        )
 
     rx_pk = re.compile(
         r"-----BEGIN (?P<type>\S*\s*)PRIVATE KEY-----"
