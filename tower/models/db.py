5cff6af9198cf06d64de9cd64f9562387c2c1621
Aleksey Shirokih <freeseacher@gmail.com>
2016-05-17 13:46:44 +0300

    detect path for db

--- tower/models/db.py
+++ tower/models/db.py
@@ -6,9 +6,14 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
+## Python modules
+from os.path import realpath, join, dirname, abspath
+
 ## Third-party packages
 from peewee import SqliteDatabase
 
-db = SqliteDatabase("var/tower/db/config.db",
+dbpath = realpath(join(dirname(abspath(__file__)), '../../../../../var/tower/db/config.db'))
+
+db = SqliteDatabase(dbpath,
                     autocommit=False, threadlocals=True)
 db.connect()
