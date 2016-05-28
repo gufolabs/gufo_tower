f78003ae790795222665ae9c073aacb196fe0998
Aleksey Shirokih <freeseacher@gmail.com>
2016-05-28 21:14:41 +0300

    improve docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,8 @@ if [ ! -f /opt/tower/var/tower/keys/id_rsa ]; then
     mkdir -p /opt/tower/var/tower/ansible/cp /opt/tower/var/tower/crashinfo
 fi
 
-exec "$@"
+if [ $# -eq 0 ]; then
+    ./bin/tower-web
+else
+    exec "$@"
+fi
