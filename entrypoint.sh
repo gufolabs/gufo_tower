182fad0c6a90f6225178fe4de6ffcdb68b1644e3
Aleksey Shirokih <freeseacher@gmail.com>
2016-05-28 18:53:00 +0300

    improve docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,13 @@ if [ ! -f /opt/tower/var/tower/keys/id_rsa ]; then
     ssh-keygen -t rsa -b 4096 -f /opt/tower/var/tower/keys/id_rsa
     chmod 0700 /opt/tower/var/tower/keys/
     cd /opt/tower
-    mkdir /opt/tower/var/tower/db /opt/tower/var/tower/cache /opt/tower/var/tower/repo
-    mkdir /opt/tower/var/tower/log/jobs /opt/tower/var/tower/log/crashinfo/collect
-    mkdir /opt/tower/var/tower/ansible/cp /opt/tower/var/tower/crashinfo
-
+    mkdir -p /opt/tower/var/tower/db /opt/tower/var/tower/cache /opt/tower/var/tower/repo
+    mkdir -p /opt/tower/var/tower/log/jobs /opt/tower/var/tower/log/crashinfo/collect
+    mkdir -p /opt/tower/var/tower/ansible/cp /opt/tower/var/tower/crashinfo
 fi
 
 if [ $# -eq 0 ]; then
-    ./bin/tower-web
+    exec ./bin/tower-web
 else
     exec "$@"
 fi
