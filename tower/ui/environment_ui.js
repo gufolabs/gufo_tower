f2d1df47286ad7ce95efb2974dedfc93abb7a836
Aleksey Shirokih <shirokih@nocproject.org>
2016-06-29 16:59:55 +0300

    permit ip address for host names

--- tower/ui/environment_ui.js
+++ tower/ui/environment_ui.js
@@ -248,10 +248,9 @@ var environment_form = {
                                 view: "text",
                                 name: "web_host",
                                 label: "Host",
-                                value: "noc",
                                 required: true,
                                 placeholder: "noc.example.com",
-                                validate: Tower.rules.regex(/^[a-zA-Z][a-zA-Z0-9\-_\.]*$/)
+                                validate: Tower.rules.regex(/^[a-zA-Z0-9\-_\.]*$/)
                             },
                             {
                                 view: "text",
