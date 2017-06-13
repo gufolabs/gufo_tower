835b81af05b504106ffbb603c7b0ca846ddd4f6d
Aleksey Shirokih <shirokih@nocproject.org>
2017-06-13 14:18:05 +0300

    fix src_dist dir creation

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ class TowerInstall(install):
         for d in ["db", "cache", "repo",
                   "log/jobs",
                   "log/crashinfo/collect",
+                  "data/src_dist",
                   "ansible/cp", "crashinfo"]:
             path = os.path.join(self.prefix, "var", "tower", d)
             if not os.path.exists(path):
