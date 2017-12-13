0f0e8dc2cd2507aac8984952c9effcbff5c904fd
Aleksey Shirokih <shirokih@nocproject.org>
2017-12-13 21:45:58 +0300

    look for db from TOWER_DB_PATH env

--- tower/models/db.py
+++ tower/models/db.py
@@ -8,11 +8,14 @@
 
 ## Python modules
 from os.path import realpath, join, dirname, abspath
+from os import environ
 
 ## Third-party packages
 from peewee import SqliteDatabase
 
 dbpath = realpath(join(dirname(abspath(__file__)), '../../../../../var/tower/db/config.db'))
+
+dbpath=environ.get("TOWER_DB_PATH", dbpath)
 
 db = SqliteDatabase(dbpath,
                     autocommit=False, threadlocals=True)
