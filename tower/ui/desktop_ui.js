63d077d317eee3c457a766b3381b86ea03a21c32
Aleksey Shirokih <shirokih@nocproject.org>
2018-03-24 02:07:04 +0300

    Bump version: 0.4.0-rc2 → 0.4.0-rc3

--- tower/ui/desktop_ui.js
+++ tower/ui/desktop_ui.js
@@ -55,7 +55,7 @@ var desktop = {
                                 {
                                     id: "version",
                                     icon: "info",
-                                    value: "Version: 0.4.0-rc2"
+                                    value: "Version: 0.4.0-rc3"
                                 }
                             ]
                         }
