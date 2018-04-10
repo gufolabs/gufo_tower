487604ba6230965c0a1d31836224ee199ddd63fc
Aleksey Shirokih <shirokih@nocproject.org>
2018-04-10 16:09:11 +0300

    revert

--- tower/ui/desktop_ui.js
+++ tower/ui/desktop_ui.js
@@ -55,7 +55,7 @@ var desktop = {
                                 {
                                     id: "version",
                                     icon: "info",
-                                    value: "Version: 0.5.0"
+                                    value: "Version: 0.4.1"
                                 }
                             ]
                         }
