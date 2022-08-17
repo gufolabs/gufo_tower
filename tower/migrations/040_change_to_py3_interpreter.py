addff0fbca0296b8216b7c3f4424f0df1861629a
EKbfh <earthquake_90@mail.ru>
2022-08-17 18:25:22 +0300

    Add python3 migration of ansible_interpreter

---
+++ tower/migrations/040_change_to_py3_interpreter.py
@@ -0,0 +1,31 @@
+# Third-party modules
+from peewee import (Model, CharField, ForeignKeyField,
+                    BooleanField)
+
+
+def migrate(migrator):
+    class NodeType(Model):
+        class Meta(object):
+            database = migrator.db
+            db_table = "node_type"
+
+        name = CharField(max_length=64, unique=True)
+        shell_type = CharField(max_length=256, default="sh")
+        python_interpreter = CharField(max_length=255,
+                                       default="/usr/bin/python3")
+        ssh_extra_args = CharField(max_length=255)
+        ssh_pipelining = BooleanField(default=False)
+        ansible_connection = CharField(max_length=255, default="smart")
+
+    d = NodeType(
+        name="Linux_py2",
+        ssh_pipelining=True,
+        python_interpreter="/usr/bin/python",
+        ansible_connection="ssh",
+    )
+    d.save()
+
+    for nt in NodeType.select():
+        if nt.name == "Linux":
+            nt.python_interpreter = "/usr/bin/python3"
+            nt.save()
